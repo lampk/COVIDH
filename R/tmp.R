a8106bc31242f94bdb2569b3431966b20d004a49
“Lam <phungkhanhlam@gmail.com>
2022-01-09 06:40:47 +0700

    update

--- R/tmp.R
+++ R/tmp.R
@@ -1,4 +1,10 @@
-data_path <- file.path(Lmisc::get.dropbox.folder(), "Workspace", "Database", "COVID19", "NCSC")
+data_path <- file.path(Lmisc::get.dropbox.folder(), "Workspace", "Database", "COVID19")
 
 get_ncsc_data(url = "https://covid.ncsc.gov.vn/api/v3/covid/province/", rawdata = file.path(data_path, "raw"), cleandata = file.path(data_path, "clean"),
                           url_vaccine = "https://covid.ncsc.gov.vn/api/v3/vaccine/national_injected_by_day?filter_type=people")
+
+get_ncsc_data(url = "https://covid.ncsc.gov.vn/api/v3/covid/province/",
+              url_vaccine = "https://covid.ncsc.gov.vn/api/v3/vaccine/national_injected_by_day?filter_type=people",
+              rawdata = file.path(data_path, "NCSC", "raw"),
+              cleandata = file.path(data_path, "NCSC", "clean"),
+              backup = file.path(data_path, "NCSC", "backup"))
